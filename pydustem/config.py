e0c0819c08c2a90b8b3364590cd93382ba6dafac
Aaron C. Bell <aaroncnb@users.noreply.github.com>
2017-03-11 12:37:27 +0900

    Update config.py
    Update dustem path

--- pydustem/config.py
+++ pydustem/config.py
@@ -1,7 +1,7 @@
 import os
 
 '''DustEM preferences'''
-DUSTEM_DIR = '/home/tnakamura/workspace/dustem3.8_web/'
+DUSTEM_DIR = '/work1/users/aaronb/Softbrary/dustem'
 OUT_DIR = os.path.join(DUSTEM_DIR, 'out')
 RES_FILE = os.path.join(OUT_DIR, 'SED.RES')
 GRAIN = os.path.join(DUSTEM_DIR, 'data/GRAIN.DAT')
@@ -16,8 +16,8 @@ FILTER_PATH = {
                  'N2'  : 'n2.fad',  'N3'  : 'n3.fad',   'N4'  : 'n4.fad',    # AKARI/IRC NIR
                  'S7'  : 's7.fad',  'S9W' : 's9w.fad',  'S11' : 's11.fad',   # AKARI/IRC MIR-S
                  'L15' : 'l15.fad', 'L18W': 'l18w.fad', 'L24' : 'l24.fad',   # AKARI/IRC MIR-L
-                 'F65' : 'f65.fad', 'F90' : 'f90.fad',  'F140': 'f140.fad',  'F160' : 'f160.fad' # AKARI/FIS
-                 'I12' : 'i12.fad', 'I25' : 'i25.fad',  'I60' : 'i60.fad',   'I100' : 'i100.fad' # IRAS 
+                 'F65' : 'f65.fad', 'F90' : 'f90.fad',  'F140': 'f140.fad',  'F160' : 'f160.fad', # AKARI/FIS
+                 'I12' : 'i12.fad', 'I25' : 'i25.fad',  'I60' : 'i60.fad',   'I100' : 'i100.fad', # IRAS 
                  'M24' : 'm24.fad', 'M70' : 'm70.fad',  'M160': 'm160.fad',  # Spitzer/MIPS
                  'P350': 'p350.fad','P550': 'p550.fad',                       # Planck/HFI
               }
